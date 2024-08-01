6905713bf9299fa592faa62be46df6ab7450a54e
YoongyuHwang <hyg2540@sweepseries.com>
2024-08-01 12:47:47 +0900

    gaza

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <footer className="footer-container">
       <div className="footer-text-container">
         <p className="footer-head">(주)스윕시리즈</p>
-        <p>사업자 등록 번호 00-000-00-00000 | 대표 홍승우</p>
+        <p>사업자 등록번호 354-87-03036 | 대표 홍승우</p>
         <p>인천 서구 청라한내로72번길 17 416호</p>
         <div className="footer-links">
           <span className="footer-link" onClick={() => window.open('https://www.notion.so/b709f41b07f64bc094926584ed0375eb', '_blank')}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-import logo from '../assets/images/logo.png'; // 로고 이미지 추가
+import catchb from '../assets/images/catchb.svg'; // 로고 이미지 추가
 import './NavBar.css'; // CSS 파일 추가
 
 const NavBar: React.FC = () => {
   return (
     <nav className="NavBar">
-      <img src={logo} alt="Logo" className="NavBar-logo" />
+      <img src={catchb} alt="catchb" className="NavBar-catchb" />
       <div className="NavBar-items">
         <Link to="appIntro" smooth={true} duration={500} className="NavBar-item">앱소개</Link>
         <Link to="events" smooth={true} duration={500} className="NavBar-item">이벤트</Link>
